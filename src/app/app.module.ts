52c75149f15e11f01bdba6376a889a4204a588e2
unknown <praneethvikram9699@gmail.com>
2021-01-02 22:12:36 +0530

    ui component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponentComponent } from './register-component/register-component.component';
+import { DashboardComponentComponent } from './dashboard-component/dashboard-component.component';
+import { CartComponentComponent } from './cart-component/cart-component.component';
+import { UserComponentComponent } from './user-component/user-component.component';
+import { ProductCardComponentComponent } from './product-card-component/product-card-component.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponentComponent,
+    DashboardComponentComponent,
+    CartComponentComponent,
+    UserComponentComponent,
+    ProductCardComponentComponent,
+    LoginComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/product-card-component/product-card-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card-component',
+  templateUrl: './product-card-component.component.html',
+  styleUrls: ['./product-card-component.component.css']
+})
+export class ProductCardComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
